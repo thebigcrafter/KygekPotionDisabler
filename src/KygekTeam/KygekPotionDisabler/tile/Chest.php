ee14e7c3bb46cca18dba68928417d07f094fb27f
Kygekraqmak <techfixindonesia@gmail.com>
2021-07-08 19:38:53 +0800

    Added license notice

--- src/KygekTeam/KygekPotionDisabler/tile/Chest.php
+++ src/KygekTeam/KygekPotionDisabler/tile/Chest.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * Unregisters and disables default PocketMine-MP regular and splash potion items
+ * Copyright (C) 2021 KygekTeam
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 declare(strict_types=1);
 
 namespace KygekTeam\KygekPotionDisabler\tile;
